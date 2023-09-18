0f6e86234bc604ecab22f65b1443941030ed94fc
Samuel Sfeir <samuel1808@hotmail.com>
2023-09-18 09:30:28 -0300

    Adiciona exercicio-02

---
+++ fundamentos/secao-03-introducao-a-javascript/Dia-01-primeiros-passos-no-javascript/Exercicio-01/exercicio-02/exercicio-02.js
@@ -0,0 +1,9 @@
+const base = 5;
+const height = 8;
+
+const area = base * height;
+
+const perimeter = 2 * (base + height);
+
+console.log("Área do retângulo:", area);
+console.log("Perímetro do retângulo:", perimeter);
