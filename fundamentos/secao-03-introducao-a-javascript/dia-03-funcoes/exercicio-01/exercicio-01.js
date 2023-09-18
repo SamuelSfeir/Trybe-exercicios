15ce31979ba3c26879997b45bb8937824a616f4d
Samuel Sfeir <samuel1808@hotmail.com>
2023-09-18 12:01:14 -0300

    Adiciona exercicio-01-dia-03

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-funcoes/exercicio-01/exercicio-01.js
@@ -0,0 +1,16 @@
+function adicionarAoBalance(balance, valor) {
+    return balance + valor;
+  }
+
+function subtrairDoBalance(balance, valor) {
+  return balance - valor;
+}
+
+function multiplicarBalancePorTaxa(balance, taxa) {
+    return balance * taxa;
+  }
+
+function dividirBalance(balance, divisor) {
+  return balance / divisor;
+}
+
