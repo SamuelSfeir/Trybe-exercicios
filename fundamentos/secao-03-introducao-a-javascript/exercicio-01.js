eac6e3477af7a870c7f7eaa48cd9a618345cef2e
Samuel Sfeir <samuel1808@hotmail.com>
2023-09-18 09:06:44 -0300

    secao-03-exercicio-01

---
+++ fundamentos/secao-03-introducao-a-javascript/exercicio-01.js
@@ -0,0 +1,7 @@
+const myName = "Samuel";
+const birthCity = "São Paulo";
+const birthYear = 1999;
+
+console.log("Nome:", myName);
+console.log("Cidade Natal:", birthCity);
+console.log("Ano de Nascimento:", birthYear);
