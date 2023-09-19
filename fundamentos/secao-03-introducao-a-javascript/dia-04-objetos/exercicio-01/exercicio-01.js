d4203d484e6df3faa1f3f2321a566e1bd30353fe
Samuel Sfeir <samuel1808@hotmail.com>
2023-09-19 08:43:16 -0300

    adiciona exercicio 2

--- fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercicio-01/exercicio-01.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-objetos/exercicio-01/exercicio-01.js
@@ -1,3 +1,5 @@
+/* -- Parte 1 -- */
+
 const player= {
     name :'Marta',
     lastName :'Silva',
@@ -8,5 +10,17 @@ const player= {
     },
     bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
 
-
 }
+
+
+/* -- Parte 2 -- */
+
+
+const achievements = player.bestInTheWorld.length
+console.log(`A jogadora ${player.name} ${player.lastName} foi eleita a melhor do mundo por ${achievements} vezes.`);
+
+
+
+/* -- Parte 3 -- */
+
+
