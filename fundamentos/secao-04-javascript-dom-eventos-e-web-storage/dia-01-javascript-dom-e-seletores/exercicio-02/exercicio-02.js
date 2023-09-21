fb26b17829523dfce9a25e9e4851690cd45c3b06
Samuel Sfeir <samuel1808@hotmail.com>
2023-09-21 11:38:45 -0300

    Adiciona commit do exercicio-02

---
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicio-02/exercicio-02.js
@@ -0,0 +1,5 @@
+const titulo = document.getElementById("header-container");
+titulo.style.backgroundColor = "rgb(0,176,105)"
+
+const emergencyTasks = document.getElementsByClassName('emergency-tasks')[0];
+emergencyTasks.style.backgroundColor = 'rgb(255, 159, 132)';
