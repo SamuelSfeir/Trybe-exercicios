89ce4824cf58c7554171c53b9e9dccae67cd00f6
Samuel Sfeir <samuel1808@hotmail.com>
2023-10-25 09:58:47 -0300

    adiciona exercicio 02

--- fundamentos/secao-07-metodos-de-array/dia-03-metodos-de-array-find-e-foreach/exercicio-02/script.js
+++ fundamentos/secao-07-metodos-de-array/dia-03-metodos-de-array-find-e-foreach/exercicio-02/script.js
@@ -1,1 +1,17 @@
-const teste = "teste"
+const cartoons = [
+    { title: 'Tom & Jerry', ageRating: 'Livre' },
+    { title: 'Scooby-Doo', ageRating: 'Livre' },
+    { title: 'Looney Tunes', ageRating: 'Livre' },
+    { title: 'The Simpsons', ageRating: 12 },
+    { title: 'Avatar: The Last Airbender', ageRating: 'Livre' },
+    { title: 'Pokémon', ageRating: 'Livre' },
+    { title: 'Dragon Ball Z', ageRating: 12 },
+    { title: 'Adventure Time', ageRating: 'Livre' },
+    { title: 'Gravity Falls', ageRating: 'Livre' },
+    { title: 'Teen Titans Go!', ageRating: 'Livre' }
+  ];
+
+  const addGender = (cartoons) => cartoons.forEach((cartoon) => cartoon.gender = 'Animação');
+
+  addGender(cartoons);
+  console.log(cartoons);
